3e84552c552ae4d56167dc30bfd45f4f03598022
k97aw02 <k97aw02@gmail.com>
2020-10-02 15:02:49 -0500

    add components

---
+++ controllers/booksController.js
@@ -0,0 +1,37 @@
+const db = require("../models");
+
+module.exports = {
+
+    findAll: (req, res) => {
+        db.Book
+            .find(req.query)
+            .sort({ date: -1 })
+            .then(dbModel => res.json(dbModel))
+            .catch(err => res.status(422).json(err))
+    },
+    findById: (req, res) => {
+        db.Book
+            .findById(req.params.id)
+            .then(dbModel => res.json(dbModel))
+            .catch(err => res.status(422).json(err))
+    },
+    save: (req, res) => {
+        db.Book
+            .create(req.body)
+            .then(dbModel => res.json(dbModel))
+            .catch(err => res.status(422).json(err))
+    },
+    update: (req, res) => {
+        db.Book
+            .findByIdAndUpdate({ _id: req.params.id }, req.body)
+            .then(dbModel => res.json(dbModel))
+            .catch(err => res.status(422).json(err))
+    },
+    remove: (req, res) => {
+        db.Book
+            .findById({ _id: req.params.id })
+            .then(dbModel => dbModel.remove())
+            .then(dbModel => res.json(dbModel))
+            .catch(err => res.status(422).json(err))
+    },
+};
---
+++ models/books.js
@@ -0,0 +1,15 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const bookSchema = new Schema({
+
+    title: { type: String, required: true },
+    authors: [{ type: String, required: true }],
+    description: String,
+    image: String,
+    link: String
+});
+
+const Books = mongoose.model("Books", bookSchema);
+
+module.exports = Books;
