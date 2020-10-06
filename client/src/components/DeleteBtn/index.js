4242a2940430c7d032443307e8eb120938e32fe6
k97aw02 <k97aw02@gmail.com>
2020-10-05 19:25:27 -0500

    styling

--- client/src/components/DeleteBtn/index.js
+++ client/src/components/DeleteBtn/index.js
@@ -1,9 +1,8 @@
 import React from "react";
 
-// Destructuring the type, className, children and onClick props, applying them to the button element
 function DeleteBtn({ type = "default", className, children, onClick }) {
     return (
-        <button onClick={onClick} className={["btn btn-lg", `btn-${type}`, className].join(" ")}>
+        <button onClick={onClick} className={["btn btn-md", `btn-${type}`, className].join(" ")}>
             {children}
         </button>
     );
--- client/src/components/Input/index.js
+++ client/src/components/Input/index.js
@@ -1,18 +1,13 @@
 import React from "react";
 
-// This component lets us use a bootstrap input element without worrying about class names
-// or manually wrapping the input with a form-group div
-// All of the props passed to this component are spread onto the input element
 export function Input(props) {
     return (
-        <div className="input-group input-group-lg">
+        <div className="input-group input-group-md">
             <input className="form-control" type="text" {...props} />
         </div>
     );
 }
 
-// SearchButton derived from 20-MERN\01-Activities\02-Stu_Recipes\Solved\client\src\components\Button\index.js
-// Destructuring the type, className, children and onClick props, applying them to the button element
 export function SearchButton({ type = "default", className, children, onClick }) {
     return (
         <button onClick={onClick} className={["search btn btn-lg", `btn-${type}`, className].join(" ")}>
